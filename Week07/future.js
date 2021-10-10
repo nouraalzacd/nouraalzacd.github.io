2fae114c80621c4586d369b7da23cc83388bcd45
nouraalzacd <69781962+nouraalzacd@users.noreply.github.com>
2021-10-10 11:56:31 -0500

    Create future.js

---
+++ Week07/future.js
@@ -0,0 +1,12 @@
+var job = ["entrepreneur", "programmer", "Lawyer", "Othodontist"];
+var geoLocation = ["Brazil", "Italy", "Australia", "Texas"];
+var spouse = ["Fahad", "Khaled", "Dawood", "Fahd"];
+var numKids = [1,0,2,3];
+
+var generator = Math.floor(Math.random() * 4);
+
+function fortune(job,geoLocation,spouse,numKids) {
+document.write("You will be a " + job + " in " + geoLocation + ", and married to " + spouse + " with " + numKids + " kids.");
+}
+
+fortune (job[generator], geoLocation[generator],spouse[generator], numKids[generator])
